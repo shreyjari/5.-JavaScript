26cb6edd1313d8742cfa69c2014894d26cca1059
Shrey Jariwala <shrey.jari@outlook.com>
2025-12-31 16:53:13 -0500

    command line tool to track time & manage time information

---
+++ 1. Basic/TimeTracker.js
@@ -0,0 +1,12 @@
+/*
+# Things to consider when making a time tracker
+1. Controls: Ask user to start, pause, stop & finish day
+2. For each New Activity ask user to:
+    - Name of activity
+    - Estimate to complete (in mins)
+    - Select a List of projects
+3. Save the response in json.
+*/
+
+const prompt = require("prompt-sync")();
+

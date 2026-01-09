c53c3112eeedee41841a792fa7c7aeb9392f0a90
Shrey Jariwala <shrey.jari@outlook.com>
2026-01-09 00:15:01 -0500

    Creating a new project that will help me select a drop in activity from the city of torono drop in sports url

---
+++ 3. Toronto Sports Drop In Program/Index.js
@@ -0,0 +1,11 @@
+/*
+This script will help me find out all the nearest locations where I can go for:
+1. Ice Skating
+2. Select a Sport
+3. Drop In program based on the current time
+
+# Setup user profile / preferences:
+1. Ask the user their Name, Birth date
+2. Sports/Activity preference
+3. Availability (Preferred times as well as a option to select Current)
+*/
